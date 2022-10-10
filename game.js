878fef375330540ef3fe9d55b75ded67084da054
Jhon Gomez <jhonjairogomez92@gmail.com>
2022-10-10 16:35:01 -0500

    Agregar canvas responsivo

--- game.js
+++ game.js
@@ -1,12 +1,13 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', starGame);
-window.addEventListener('resize', starGame);
+let canvasSize;
+let elementsSize;
 
-function starGame() {
-  let canvasSize;
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
 
+function setCanvasSize() {
   if (window.innerHeight > window.innerWidth) {
     canvasSize = window.innerWidth * 0.8;
   } else {
@@ -16,7 +17,12 @@ function starGame() {
   canvas.setAttribute('height', canvasSize);
   canvas.setAttribute('width', canvasSize);
 
-  const elementsSize = canvasSize / 10;
+  elementsSize = canvasSize / 10;
+
+  startGame();
+}
+
+function startGame() {
 
   game.font = elementsSize + 'px Verdana';
   game.textAlign = 'end';
@@ -24,8 +30,4 @@ function starGame() {
   for (let i = 1; i <= 10; i++) {
     game.fillText(emojis['X'], elementsSize, elementsSize * i);
   }
-
-  console.log('heig', window.innerHeight);
-  console.log('wid', window.innerWidth);
-
 }
